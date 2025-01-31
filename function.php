242644f9f525708ab591b96115394d1d50df2fff
devssabbir <devssabbir@gmail.com>
2025-01-31 23:47:41 +0600

    custom-search-form-with-style

--- function.php
+++ function.php
@@ -6,29 +6,28 @@ function custom_product_search_form() {
     ));
 
     // Start the form
-    $form = '<form role="search" method="get" class="inline-search-form" action="' . esc_url(home_url('/')) . '">
-                <div class="inline-search-container">
-                    <div class="inline-search-field category-field">
-               
-                        <select name="product_cat" id="category">
-                            <option value="">All</option>';
+    $form = '<form role="search" method="get" class="amazon-like-search-form" action="' . esc_url(home_url('/')) . '">
+                <div class="search-container">
+                    <div class="category-dropdown">
+                        <select name="product_cat" id="category">';
 
-    // Add category options
+    // Add category options (without "All Categories")
     foreach ($categories as $category) {
         $form .= '<option value="' . esc_attr($category->slug) . '">' . esc_html($category->name) . '</option>';
     }
 
-    // Add search input and button
+    // Add search input and icon button
     $form .= '</select>
                     </div>
-                    <div class="inline-search-field input-field">
-                      
+                    <div class="search-input-wrapper">
                         <input type="text" name="s" id="search" placeholder="Enter product name or keyword">
                     </div>
-                    <div class="inline-search-field button-field">
-                        <input type="hidden" name="post_type" value="product"> <!-- Limit search to products -->
-                        <button type="submit">Search</button>
+                    <div class="search-button">
+                        <button type="submit">
+                            <i class="fas fa-search"></i> <!-- FontAwesome search icon -->
+                        </button>
                     </div>
+                    <input type="hidden" name="post_type" value="product"> <!-- Limit search to products -->
                 </div>
             </form>';
 
@@ -37,3 +36,8 @@ function custom_product_search_form() {
 
 // Shortcode to display the form
 add_shortcode('custom_product_search', 'custom_product_search_form');
+
+function enqueue_fontawesome() {
+    wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_fontawesome');
